33c6207d4626c67cd3b855c3e732b0a22704ca16
hasanarofid <hasanarofid@gmail.com>
2026-08-24 13:19:16 +0700

    fix: register Spatie Permission middleware alias 'role' in bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,13 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->trustProxies(at: '*');
 
+        // Register Spatie Permission Middleware Aliases
+        $middleware->alias([
+            'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+            'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+            'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
+        ]);
+
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
